a174bfebdff6f161e5a73819d1f96e803e937746
alaneric1618 <alaneric1618@gmail.com>
2014-02-22 18:45:09 -0600

    Updated lexer and included some help docs

--- lexer.rb
+++ lexer.rb
@@ -13,7 +13,7 @@ class Lexer
     parse(file)
   end
 
-  def parse(file) 
+  def parse(file)
     text = file.read
     #remove comments
     while (text =~ /\/\/.*$/)
@@ -22,6 +22,7 @@ class Lexer
     #tokenize
     regtokens = /[a-zA-Z][\w\d]*|:=|\+|\-|\*|\(|\)|<=|=|\d+|\/|;|true|false|and|not|skip|if|then|else|while|do/
     @tokens = text.scan(regtokens)
+    @tokens.insert(-1,"EOF")
   end
 
   def print_tokens()
@@ -45,55 +46,172 @@ class Lexer
 
 end
 
-
-
-class Taxer
-  @error = false
+class Parser
   @lexer
+  @current_symbol
+  @symbols
 
   def initialize(lexer)
-    @error = false
     @lexer = lexer
+    @symbols = {
+      "plus"       => 1,
+      "minus"      => 2,
+      "lteq"       => 3,
+      "equal"      => 4,
+      "multiply"   => 5,
+      "divide"     => 6,
+      "true"       => 7,
+      "false"      => 8,
+      "and"        => 9,
+      "not"        => 10,
+      "if"         => 11,
+      "then"       => 12,
+      "else"       => 13,
+      "while"      => 14,
+      "do"         => 15,
+      "skip"       => 16,
+      "identifier" => 17,
+      "leftparen"  => 18,
+      "rightparen" => 19,
+      "integer"    => 20,
+      ";"          => 21,
+      "assign"     => 22,
+      "EOF"        => 23
+    }
+    get_symbol()
+    program
   end
 
-  def analyze()
-    descend(@lexer)
-  end
-
-  def descend(lexer)
-    if (token = lexer.get_token)
-      if (!token_good(token, lexer))
-        @error = true
-      end
-      descend(lexer)
+  def get_symbol()
+    token = @lexer.get_token
+    puts token
+    case token
+    when /^\+$/
+      @current_symbol = @symbols["plus"]
+    when /^\-$/
+      @current_symbol = @symbols["minus"]
+    when /^<=$/
+      @current_symbol = @symbols["lteq"]
+    when /^=$/
+      @current_symbol = @symbols["equal"]
+    when /^\*$/
+      @current_symbol = @symbols["multiply"]
+    when /^\/$/
+      @current_symbol = @symbols["divide"]
+    when /^true$/
+      @current_symbol = @symbols["true"]
+    when /^false$/
+      @current_symbol = @symbols["false"]
+    when /^and$/
+      @current_symbol = @symbols["and"]
+    when /^not$/
+      @current_symbol = @symbols["not"]
+    when /^if$/
+      @current_symbol = @symbols["if"]
+    when /^then$/
+      @current_symbol = @symbols["then"]
+    when /^else$/
+      @current_symbol = @symbols["else"]
+    when /^while$/
+      @current_symbol = @symbols["while"]
+    when /^do$/
+      @current_symbol = @symbols["do"]
+    when /^skip$/
+      @current_symbol = @symbols["skip"]
+    when /^[a-zA-Z][\w\d]*$/
+      @current_symbol = @symbols["identifier"]
+    when /^\($/
+      @current_symbol = @symbols["leftparen"]
+    when /^\)$/
+      @current_symbol = @symbols["rightparen"]
+    when /^\d+$/
+      @current_symbol = @symbols["integer"]
+    when /^;$/
+      @current_symbol = @symbols[";"]
+    when /^:=$/
+      @current_symbol = @symbols["assign"]
+    when /^EOF$/
+      @current_symbol = @symbols["EOF"]
     else
-      if (@error)
-        puts "Syntax Error"
-      else
-        puts "Syntax formed correctly!!!"
-      end
+
+    end
+
+  end
+
+  def accept(symbol)
+    if (symbol == @current_symbol)
+      get_symbol()
+      return true
+    end
+    return false
+  end
+
+  def expect(symbol)
+    if (accept symbol)
+      return true
+    end
+    error("Error: unexpected symbol: #{@current_symbol}")
+    return false
+  end
+
+  def error(message)
+    puts message
+  end
+
+  def expr()
+    term()
+    
+  end
+
+  def stmt()
+    if (accept(@symbols["skip"]))
+
+    elsif (accept(@symbols["identifier"]))
+      expect(@symbols["assign"])
+      expr()
+    elsif (accept(@symbols["if"]))
+      expect(@symbols["leftparen"])
+      lexpr()
+      expect(@symbols["rightparen"])
+      expect(@symbols["then"])
+      expect(@symbols["leftparen"])
+      stmts()
+      expect(@symbols["rightparen"])
+      expect(@symbols["else"])
+      expect(@symbols["leftparen"])
+      stmts()
+      expect(@symbols["rightparen"])
+    elsif (accept(@symbols["while"]))
+      expect(@symbols["leftparen"])
+      lexpr()
+      expect(@symbols["rightparen"])
+      expect(@symbols["do"])
+      expect(@symbols["leftparen"])
+      stmts()
+      expect(@symbols["rightparen"])
+    else
+      error("Parse Error: Expected a statement")
     end
   end
 
-  def token_good(token, lexer)
-    error = false
-    if (token == "if")
-      error = error || lexer.get_token != "("
+  def stmts()
+    stmt()
+    while (@current_symbol == @symbols[";"])
+      get_symbol()
+      stmts()
     end
-
-    return true #!error
   end
 
-  def skip?
-
+  def program()
+    stmts()
+    expect(@symbols["EOF"])
   end
-
 
 end
 
 lexer = Lexer.new("test.while")
 lexer.print_tokens
-taxer = Taxer.new(lexer)
-taxer.analyze
+parser = Parser.new(lexer)
+
 
 
